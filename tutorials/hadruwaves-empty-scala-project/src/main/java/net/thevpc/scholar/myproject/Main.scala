58175ac04c7b08c2d3a6084ae5697d03d78d54b9
thevpc <taha.bensalah@gmail.com>
2024-11-27 10:08:47 +0100

    [BUMPED] bumped to nut 0.8.5

---
+++ tutorials/hadruwaves-empty-scala-project/src/main/java/net/thevpc/scholar/myproject/Main.scala
@@ -0,0 +1,19 @@
+package net.thevpc.scholar.myproject
+
+import java.io.{File, PrintStream}
+
+import net.thevpc.common.io.IOUtils
+import net.thevpc.scholar.hadrumaths.MathScala._
+import net.thevpc.scholar.hadrumaths._
+import net.thevpc.scholar.hadrumaths.io.HadrumathsIOUtils
+import net.thevpc.scholar.hadrumaths.scalarproducts.MemComplexScalarProductCache
+import net.thevpc.scholar.hadruplot.Plot
+import net.thevpc.scholar.hadruwaves.Material
+import net.thevpc.scholar.hadruwaves.mom.BoxSpace._
+import net.thevpc.scholar.hadruwaves.mom.modes.SimpleModeIterator
+import net.thevpc.scholar.hadruwaves.mom.{HintAxisType, MomStructure}
+
+
+object Main {
+
+}
