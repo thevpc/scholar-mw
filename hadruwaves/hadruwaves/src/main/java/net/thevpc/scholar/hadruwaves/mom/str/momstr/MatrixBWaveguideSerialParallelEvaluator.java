b2ec0f52465bcf29357a5aa4ac92bddcfb4afbc7
thevpc <taha.bensalah@gmail.com>
2025-11-25 08:39:13 +0100

    Update MatrixBWaveguideSerialParallelEvaluator.java : bu,p-nuts-v0.8.9

--- hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/mom/str/momstr/MatrixBWaveguideSerialParallelEvaluator.java
+++ hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/mom/str/momstr/MatrixBWaveguideSerialParallelEvaluator.java
@@ -2,9 +2,9 @@ package net.thevpc.scholar.hadruwaves.mom.str.momstr;
 
 import net.thevpc.common.mon.ProgressMonitors;
 import net.thevpc.common.mon.VoidMonitoredAction;
-import net.thevpc.tson.Tson;
-import net.thevpc.tson.TsonElement;
-import net.thevpc.tson.TsonObjectContext;
+
+import net.thevpc.nuts.elem.NElement;
+
 import net.thevpc.scholar.hadrumaths.*;
 import net.thevpc.scholar.hadrumaths.symbolic.DoubleToVector;
 import net.thevpc.common.mon.ProgressMonitor;
@@ -73,7 +73,7 @@ public class MatrixBWaveguideSerialParallelEvaluator implements MatrixBEvaluator
     }
 
     @Override
-    public TsonElement toTsonElement(TsonObjectContext context) {
-        return Tson.ofUplet(getClass().getSimpleName()).build();
+    public NElement toElement() {
+        return NElement.ofUplet(getClass().getSimpleName());
     }
 }
