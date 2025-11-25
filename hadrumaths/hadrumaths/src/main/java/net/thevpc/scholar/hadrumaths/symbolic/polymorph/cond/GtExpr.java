91c9201ed4b52808071c8c809b5dfdb70d218499
thevpc <taha.bensalah@gmail.com>
2025-11-25 08:39:09 +0100

    Update GtExpr.java : bu,p-nuts-v0.8.9

--- hadrumaths/hadrumaths/src/main/java/net/thevpc/scholar/hadrumaths/symbolic/polymorph/cond/GtExpr.java
+++ hadrumaths/hadrumaths/src/main/java/net/thevpc/scholar/hadrumaths/symbolic/polymorph/cond/GtExpr.java
@@ -1,5 +1,6 @@
 package net.thevpc.scholar.hadrumaths.symbolic.polymorph.cond;
 
+import net.thevpc.nuts.elem.NElement;
 import net.thevpc.scholar.hadrumaths.Expr;
 import net.thevpc.scholar.hadrumaths.symbolic.DoubleToDouble;
 
@@ -12,6 +13,12 @@ public class GtExpr extends AbstractComparatorExpr {
     private GtExpr(DoubleToDouble xarg, DoubleToDouble yarg) {
         super(xarg, yarg, xarg.getDomain().intersect(yarg.getDomain()));
     }
+
+    @Override
+    public NElement toElement() {
+        return NElement.ofNamedObject("GtExpr");
+    }
+
 
     @Override
     public String getOperatorName() {
