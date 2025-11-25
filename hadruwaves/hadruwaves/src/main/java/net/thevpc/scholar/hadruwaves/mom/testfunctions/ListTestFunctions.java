26d5d32b272b6bb297758bd0d4e8a7e3684c6872
thevpc <taha.bensalah@gmail.com>
2025-11-25 08:39:14 +0100

    Update ListTestFunctions.java : bu,p-nuts-v0.8.9

--- hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/mom/testfunctions/ListTestFunctions.java
+++ hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/mom/testfunctions/ListTestFunctions.java
@@ -7,14 +7,16 @@ package net.thevpc.scholar.hadruwaves.mom.testfunctions;
 
 import net.thevpc.common.mon.MonitoredAction;
 import net.thevpc.common.mon.ProgressMonitor;
-import net.thevpc.tson.TsonElement;
-import net.thevpc.tson.TsonObjectBuilder;
-import net.thevpc.tson.TsonObjectContext;
+import net.thevpc.nuts.elem.NElement;
+
+
+import net.thevpc.nuts.elem.NObjectElementBuilder;
 import net.thevpc.scholar.hadrumaths.Expr;
 import net.thevpc.scholar.hadrumaths.Maths;
 import net.thevpc.scholar.hadrumaths.Vector;
 import net.thevpc.scholar.hadrumaths.geom.Geometry;
 import net.thevpc.scholar.hadrumaths.symbolic.DoubleToVector;
+import net.thevpc.scholar.hadrumaths.util.NElementHelper;
 import net.thevpc.scholar.hadruwaves.mom.HintAxisType;
 import net.thevpc.scholar.hadruwaves.mom.MomStructure;
 import net.thevpc.scholar.hadruwaves.mom.TestFunctions;
@@ -201,9 +203,9 @@ public class ListTestFunctions extends TestFunctionsBase implements Cloneable {
     }
 
     @Override
-    public TsonElement toTsonElement(TsonObjectContext context) {
-        TsonObjectBuilder h = super.toTsonElement(context).toObject().builder();
-        h.add("functions", context.elem(list));
+    public NElement toElement() {
+        NObjectElementBuilder h = super.toElement().toObject().get().builder();
+        h.add("functions", NElementHelper.elem(list));
         return h.build();
     }
 
