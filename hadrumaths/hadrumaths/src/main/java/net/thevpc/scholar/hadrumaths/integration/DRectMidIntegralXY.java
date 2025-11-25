62008184ccc2dc9c7ba2ee9de10e372017804a5a
thevpc <taha.bensalah@gmail.com>
2025-11-25 08:39:07 +0100

    Update DRectMidIntegralXY.java : bu,p-nuts-v0.8.9

--- hadrumaths/hadrumaths/src/main/java/net/thevpc/scholar/hadrumaths/integration/DRectMidIntegralXY.java
+++ hadrumaths/hadrumaths/src/main/java/net/thevpc/scholar/hadrumaths/integration/DRectMidIntegralXY.java
@@ -1,8 +1,8 @@
 package net.thevpc.scholar.hadrumaths.integration;
 
-import net.thevpc.tson.Tson;
-import net.thevpc.tson.TsonElement;
-import net.thevpc.tson.TsonObjectContext;
+
+import net.thevpc.nuts.elem.NElement;
+
 import net.thevpc.scholar.hadrumaths.symbolic.DoubleToDouble;
 
 /**
@@ -173,9 +173,9 @@ public class DRectMidIntegralXY implements DIntegralXY {
     }
 
     @Override
-    public TsonElement toTsonElement(TsonObjectContext context) {
-        return Tson.ofUplet(getClass().getSimpleName(), Tson.of(xprecision), Tson.of(yprecision), Tson.of(zprecision))
-                .build();
+    public NElement toElement() {
+        return NElement.ofUplet(getClass().getSimpleName(), NElement.ofInt(xprecision), NElement.ofInt(yprecision), NElement.ofInt(zprecision))
+                ;
     }
 //    @Override
 //    public String dump() {
