fe59f52c290cf2d5889f756bf32fc1d54ba9e64f
thevpc <taha.bensalah@gmail.com>
2025-11-25 08:39:15 +0100

    Update HWSolutionElement.java : bu,p-nuts-v0.8.9

--- hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/project/HWSolutionElement.java
+++ hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/project/HWSolutionElement.java
@@ -3,9 +3,9 @@ package net.thevpc.scholar.hadruwaves.project;
 import net.thevpc.common.props.ObservableValue;
 import net.thevpc.common.props.WritableString;
 import net.thevpc.common.props.WritableValue;
-import net.thevpc.tson.TsonSerializable;
+import net.thevpc.nuts.elem.NToElement;
 
-public interface HWSolutionElement extends TsonSerializable{
+public interface HWSolutionElement extends NToElement{
     WritableString name();
 
     WritableString description();
