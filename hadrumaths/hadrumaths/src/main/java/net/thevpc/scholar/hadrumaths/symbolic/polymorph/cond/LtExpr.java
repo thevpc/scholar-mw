8cca67ae2fbf0baf52ae3ed24dd4d4ea063c2966
thevpc <taha.bensalah@gmail.com>
2025-11-25 08:39:10 +0100

    Update LtExpr.java : bu,p-nuts-v0.8.9

--- hadrumaths/hadrumaths/src/main/java/net/thevpc/scholar/hadrumaths/symbolic/polymorph/cond/LtExpr.java
+++ hadrumaths/hadrumaths/src/main/java/net/thevpc/scholar/hadrumaths/symbolic/polymorph/cond/LtExpr.java
@@ -1,5 +1,6 @@
 package net.thevpc.scholar.hadrumaths.symbolic.polymorph.cond;
 
+import net.thevpc.nuts.elem.NElement;
 import net.thevpc.scholar.hadrumaths.Expr;
 import net.thevpc.scholar.hadrumaths.symbolic.DoubleToDouble;
 
@@ -12,6 +13,12 @@ public class LtExpr extends AbstractComparatorExpr {
     private LtExpr(DoubleToDouble xarg, DoubleToDouble yarg) {
         super(xarg, yarg, xarg.getDomain().intersect(yarg.getDomain()));
     }
+
+    @Override
+    public NElement toElement() {
+        return NElement.ofNamedObject("LtExpr");
+    }
+
 
     @Override
     public String getOperatorName() {
