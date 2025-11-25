c844c7db08f166b6ee30edf902aeab9aaeb3b9b9
thevpc <taha.bensalah@gmail.com>
2025-11-25 08:39:15 +0100

    Update Element3DTemplate.java : bu,p-nuts-v0.8.9

--- hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/project/scene/Element3DTemplate.java
+++ hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/project/scene/Element3DTemplate.java
@@ -1,9 +1,9 @@
 package net.thevpc.scholar.hadruwaves.project.scene;
 
-import net.thevpc.tson.TsonSerializable;
+import net.thevpc.nuts.elem.NToElement;
 import net.thevpc.scholar.hadruplot.libraries.calc3d.elements.Element3D;
 import net.thevpc.scholar.hadruwaves.project.configuration.HWConfigurationRun;
 
-public interface Element3DTemplate extends TsonSerializable{
+public interface Element3DTemplate extends NToElement{
     Element3D toElements3D(HWConfigurationRun configuration);
 }
