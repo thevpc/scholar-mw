8782581dd70843df2a50568afcd9b931eb58583c
thevpc <taha.bensalah@gmail.com>
2025-11-25 08:39:06 +0100

    Update HSerializable.java : bu,p-nuts-v0.8.9

--- hadrumaths/hadrumaths/src/main/java/net/thevpc/scholar/hadrumaths/HSerializable.java
+++ hadrumaths/hadrumaths/src/main/java/net/thevpc/scholar/hadrumaths/HSerializable.java
@@ -1,18 +1,18 @@
 package net.thevpc.scholar.hadrumaths;
 
-import net.thevpc.tson.TsonSerializable;
+import net.thevpc.nuts.elem.NToElement;
 import net.thevpc.scholar.hadrumaths.util.dump.Dumpable;
 
 import java.io.Serializable;
 
-public interface HSerializable extends TsonSerializable, Serializable, Dumpable {
+public interface HSerializable extends NToElement, Serializable, Dumpable {
 
     @Override
     String toString();
 
     @Override
     default String dump() {
-        return toTsonElement().toString(false);
+        return toElement().toString(false);
     }
 
 }
