cd5a1082046f197c3724226ebabf2b415704fad3
thevpc <taha.bensalah@gmail.com>
2025-11-25 08:39:13 +0100

    Update GpAlternateXYComparator.java : bu,p-nuts-v0.8.9

--- hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/mom/str/GpAlternateXYComparator.java
+++ hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/mom/str/GpAlternateXYComparator.java
@@ -1,8 +1,8 @@
 package net.thevpc.scholar.hadruwaves.mom.str;
 
-import net.thevpc.tson.Tson;
-import net.thevpc.tson.TsonElement;
-import net.thevpc.tson.TsonObjectContext;
+
+import net.thevpc.nuts.elem.NElement;
+
 import net.thevpc.scholar.hadrumaths.symbolic.DoubleToVector;
 
 import java.io.Serializable;
@@ -36,8 +36,8 @@ public class GpAlternateXYComparator implements TestFunctionsComparator, Seriali
     }
 
     @Override
-    public TsonElement toTsonElement(TsonObjectContext context) {
-        return Tson.ofUplet(getClass().getSimpleName()).build();
+    public NElement toElement() {
+        return NElement.ofUplet(getClass().getSimpleName());
     }
 
     @Override
