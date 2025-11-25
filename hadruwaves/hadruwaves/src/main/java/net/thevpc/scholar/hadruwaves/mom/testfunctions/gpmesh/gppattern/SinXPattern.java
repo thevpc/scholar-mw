7ad492a0888688e0f91a9490cbaeefa0362e2092
thevpc <taha.bensalah@gmail.com>
2025-11-25 08:39:14 +0100

    Update SinXPattern.java : bu,p-nuts-v0.8.9

--- hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/mom/testfunctions/gpmesh/gppattern/SinXPattern.java
+++ hadruwaves/hadruwaves/src/main/java/net/thevpc/scholar/hadruwaves/mom/testfunctions/gpmesh/gppattern/SinXPattern.java
@@ -1,14 +1,16 @@
 package net.thevpc.scholar.hadruwaves.mom.testfunctions.gpmesh.gppattern;
 
-import net.thevpc.tson.TsonElement;
-import net.thevpc.tson.TsonObjectBuilder;
-import net.thevpc.tson.TsonObjectContext;
+import net.thevpc.nuts.elem.NElement;
+
+
+import net.thevpc.nuts.elem.NObjectElementBuilder;
 import net.thevpc.scholar.hadrumaths.Expr;
 import net.thevpc.scholar.hadrumaths.Domain;
 import net.thevpc.scholar.hadrumaths.FunctionFactory;
 import net.thevpc.scholar.hadrumaths.Maths;
 import net.thevpc.scholar.hadrumaths.symbolic.DoubleToVector;
 import net.thevpc.scholar.hadrumaths.meshalgo.MeshZone;
+import net.thevpc.scholar.hadrumaths.util.NElementHelper;
 import net.thevpc.scholar.hadruwaves.mom.MomStructure;
 
 import static java.lang.Math.PI;
@@ -26,9 +28,9 @@ public final class SinXPattern extends RectMeshAttachGpPattern {
     }
 
     @Override
-    public TsonElement toTsonElement(TsonObjectContext context) {
-        TsonObjectBuilder h = super.toTsonElement(context).toObject().builder();
-        h.add("max", context.elem(max));
+    public NElement toElement() {
+        NObjectElementBuilder h = super.toElement().toObject().get().builder();
+        h.add("max", NElementHelper.elem(max));
         return h.build();
     }
 
