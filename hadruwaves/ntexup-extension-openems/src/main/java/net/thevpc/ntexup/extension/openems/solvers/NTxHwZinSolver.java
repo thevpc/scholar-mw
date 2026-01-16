7a432fcad03b035953df0564a33007bc9248de28
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:33:47 +0100

    Update NTxHwZinSolver.java : better-ntexup

---
+++ hadruwaves/ntexup-extension-openems/src/main/java/net/thevpc/ntexup/extension/openems/solvers/NTxHwZinSolver.java
@@ -0,0 +1,17 @@
+package net.thevpc.ntexup.extension.openems.solvers;
+
+import net.thevpc.ntexup.extension.mwsimulator.NTxStrSimulationQuery;
+import net.thevpc.ntexup.extension.mwsimulator.NTxTargetSolver;
+import net.thevpc.nuts.command.NExec;
+import net.thevpc.nuts.io.NPath;
+
+public class NTxHwZinSolver implements NTxTargetSolver {
+    @Override
+    public Object solve(NTxStrSimulationQuery query) {
+        NPath path = NPath.ofTempFile("openems.xml");
+        NExec cmd = NExec.ofSystem("openEMS", "-f", path.toString());
+        if (false) {
+            cmd.run();
+        }
+        return 1;    }
+}
