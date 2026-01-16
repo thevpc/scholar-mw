523c4dd862bd0c5e05b78f6386ffb7a6f949501a
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:33:46 +0100

    Update MoMStrSimulationQuery.java : better-ntexup

---
+++ hadruwaves/ntexup-extension-hadruwaves/src/main/java/net/thevpc/ntexup/extension/hadruwaves/MoMStrSimulationQuery.java
@@ -0,0 +1,8 @@
+package net.thevpc.ntexup.extension.hadruwaves;
+
+import net.thevpc.ntexup.extension.mwsimulator.NTxStrSimulationQuery;
+import net.thevpc.scholar.hadruwaves.mom.MomStructure;
+
+public class MoMStrSimulationQuery extends NTxStrSimulationQuery {
+    public MomStructure str;
+}
