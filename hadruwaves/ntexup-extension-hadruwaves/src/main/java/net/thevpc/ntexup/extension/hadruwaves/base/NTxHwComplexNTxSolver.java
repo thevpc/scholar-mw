c2f80f2bcf4f621e9dd38ff57f19a1e96f478add
thevpc <taha.bensalah@gmail.com>
2026-03-26 13:51:54 +0100

    Update NTxHwComplexNTxSolver.java : update

--- hadruwaves/ntexup-extension-hadruwaves/src/main/java/net/thevpc/ntexup/extension/hadruwaves/base/NTxHwComplexNTxSolver.java
+++ hadruwaves/ntexup-extension-hadruwaves/src/main/java/net/thevpc/ntexup/extension/hadruwaves/base/NTxHwComplexNTxSolver.java
@@ -1,36 +1,40 @@
 package net.thevpc.ntexup.extension.hadruwaves.base;
 
+import net.thevpc.ntexup.api.eval.NTxObj;
+import net.thevpc.ntexup.api.eval.NTxObjs;
 import net.thevpc.ntexup.extension.hadruwaves.MoMStrNTxSimulationPlan;
 import net.thevpc.ntexup.extension.mwsimulator.*;
+import net.thevpc.nuts.elem.NArrayElement;
 import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.elem.NElements;
 import net.thevpc.nuts.elem.NUpletElementBuilder;
 import net.thevpc.nuts.math.NDoubleComplex;
 import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.time.NChronometer;
+import net.thevpc.nuts.util.NOptional;
 import net.thevpc.scholar.hadrumaths.Complex;
 import net.thevpc.scholar.hadruplot.Plot;
 import net.thevpc.scholar.hadruwaves.mom.MomStructure;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public abstract class NTxHwComplexNTxSolver extends NTxSolverRunImpl {
     private NTxSweep sweep;
     private NTxSweepTarget sweepParam;
 
-    public NTxHwComplexNTxSolver(MoMStrNTxSimulationPlan moMStrSimulationQuery, String computeName, String solverName, String solverType) {
-        super(computeName, solverName, solverType,moMStrSimulationQuery);
+    public NTxHwComplexNTxSolver(MoMStrNTxSimulationPlan plan, String computeName, String solverName, String solverType) {
+        super(computeName, solverName, solverType, plan);
     }
 
     @Override
     public void addImpl(String paramName, NElement paramValue) {
-        switch (paramName){
+        switch (paramName) {
             case "freq":
             case "frequency":
-            case "frequencies":{
-                sweepParam=NTxSweepTarget.FREQ;
-                sweep=NTxSweep.parse(paramValue).orNull();
+            case "frequencies": {
+                sweepParam = NTxSweepTarget.FREQ;
+                sweep = NTxSweep.parse(paramValue).orNull();
                 break;
             }
         }
@@ -39,8 +43,8 @@ public abstract class NTxHwComplexNTxSolver extends NTxSolverRunImpl {
     @Override
     public NElement toElement() {
         NUpletElementBuilder b = NElement.ofUpletBuilder(outputName());
-        b.add(NElement.ofPair("solver",solverType()));
-        if(sweepParam !=null){
+        b.add(NElement.ofPair("solver", solverType()));
+        if (sweepParam != null) {
             b.add(NElement.ofPair("param", sweepParam.name()));
             b.add(NElement.ofPair("sweep", sweep.toElement()));
         }
@@ -49,22 +53,28 @@ public abstract class NTxHwComplexNTxSolver extends NTxSolverRunImpl {
 
     @Override
     public List<NTxSimulationResult> execute() {
-        MomStructure str = ((MoMStrNTxSimulationPlan) query()).str;
+        MomStructure str = ((MoMStrNTxSimulationPlan) plan()).str;
         NChronometer chronometer = NChronometer.startNow();
         List<Number> complexValues = new ArrayList<>();
+        Number[] x = new Number[0];
         if (sweep != null) {
             switch (sweepParam) {
-                case FREQ:
-                {
+                case FREQ: {
                     str.log().log(NMsg.ofC("------------------"));
-                    str.log().log(NMsg.ofC("[%s] %s (%s over %s): ", outputName(), solverName(),sweepParam,sweep));
+                    str.log().log(NMsg.ofC("[%s] %s (%s over %s): ", outputName(), solverName(), sweepParam, sweep));
                     str.log().log(NMsg.ofC("------------------"));
-                    for (double fr : sweep.doubleValues()) {
+                    double[] dv = sweep.doubleValues();
+                    for (double fr : dv) {
                         str.setFrequency(fr);
                         Complex c = evalComplex(str);
                         complexValues.add(NDoubleComplex.of(c.getReal(), c.getImag()));
                     }
+                    x = Arrays.stream(dv).boxed().toArray(Number[]::new);
                     break;
+                }
+                default: {
+                    x=new Number[]{0.0};
+                    complexValues.add(0.0);
                 }
             }
         } else {
@@ -75,10 +85,28 @@ public abstract class NTxHwComplexNTxSolver extends NTxSolverRunImpl {
             complexValues.add(
                     NDoubleComplex.of(c.getReal(), c.getImag())
             );
+            x = new Number[]{0.0};
         }
-        Plot.title(solverType()+"-"+ outputName()).plot(complexValues.toArray(new Number[0]));
-        str.log().log(NMsg.ofC("[%s] %s Finished in %s : ", outputName(), solverName(),chronometer.stop()));
-        return Arrays.asList(
+        Number[] y = complexValues.toArray(new Number[0]);
+        NArrayElement ye = NElement.ofArrayBuilder()
+                .addAll(
+                        Arrays.stream(y).map(xx -> {
+                            if (xx instanceof Complex) {
+                                return NElement.ofDoubleComplex(((Complex) xx).getReal(), ((Complex) xx).getImag());
+                            }
+                            return NElements.of().toElement(xx);
+                        }).collect(Collectors.toList())
+                ).build();
+        plan().rendererContext().compiledDocument().setGlobalObject(outputName(), NTxObjs
+                .map()
+                .set("x", NElement.ofArray(
+                        Arrays.stream(x).map(xx->NElement.ofNumber(xx))
+                ))
+                .set("y", ye)
+        );
+        Plot.title(solverType() + "-" + outputName()).plot(y);
+        str.log().log(NMsg.ofC("[%s] %s Finished in %s : ", outputName(), solverName(), chronometer.stop()));
+        return Collections.singletonList(
                 NTxSimulationResultFactory.createPlot2dCurve(outputName(), sweep, complexValues)
         );
     }
