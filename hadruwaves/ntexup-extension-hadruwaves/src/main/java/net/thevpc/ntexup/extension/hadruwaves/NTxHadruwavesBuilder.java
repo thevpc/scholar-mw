4f32978f806b62048dc968be6950c00a62351ef3
thevpc <taha.bensalah@gmail.com>
2026-03-26 13:51:54 +0100

    Update NTxHadruwavesBuilder.java : update

--- hadruwaves/ntexup-extension-hadruwaves/src/main/java/net/thevpc/ntexup/extension/hadruwaves/NTxHadruwavesBuilder.java
+++ hadruwaves/ntexup-extension-hadruwaves/src/main/java/net/thevpc/ntexup/extension/hadruwaves/NTxHadruwavesBuilder.java
@@ -1,10 +1,14 @@
 package net.thevpc.ntexup.extension.hadruwaves;
 
+import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
 import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
 import net.thevpc.ntexup.api.parser.NTxAllArgumentReader;
 import net.thevpc.ntexup.api.renderer.NTxRendererContext;
 import net.thevpc.ntexup.extension.mwsimulator.*;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.elem.NElementFormatter;
+import net.thevpc.nuts.text.NContentType;
 import net.thevpc.scholar.hadruwaves.mom.MomStructure;
 
 
@@ -19,10 +23,14 @@ public class NTxHadruwavesBuilder implements NTxNodeBuilder {
                 .parseParam()
                 .matchesAny().end()
                 .processChildren(this::processChildren)
+                .withToElem(this::toElement)
                 .renderComponent(this::renderMain)
         ;
     }
 
+    protected NElement toElement(NTxNode item, boolean semantic, NTxNodeBuilderContext buildContext){
+        return item.getRaw().format(NContentType.TSON, NElementFormatter.ofPretty().builder().removeComments().build());
+    }
     public void processChildren(NTxAllArgumentReader info, NTxNodeBuilderContext buildContext) {
         //info.node().setUserObject("def", all);
     }
@@ -34,7 +42,7 @@ public class NTxHadruwavesBuilder implements NTxNodeBuilder {
                     if (str == null) {
                         return null;
                     }
-                    return new MoMStrNTxSimulationPlan(name,rendererContext.log(), str);
+                    return new MoMStrNTxSimulationPlan(name,rendererContext, str);
                 }
         );
     }
