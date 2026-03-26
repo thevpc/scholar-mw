990257d7245f77ad888ae61360a8cd39e12a51c1
thevpc <taha.bensalah@gmail.com>
2026-03-26 13:51:54 +0100

    Update NTxSimulationPlan.java : update

--- hadruwaves/ntexup-extension-mw-simulator/src/main/java/net/thevpc/ntexup/extension/mwsimulator/NTxSimulationPlan.java
+++ hadruwaves/ntexup-extension-mw-simulator/src/main/java/net/thevpc/ntexup/extension/mwsimulator/NTxSimulationPlan.java
@@ -1,8 +1,11 @@
 package net.thevpc.ntexup.extension.mwsimulator;
 
+import net.thevpc.ntexup.api.renderer.NTxRendererContext;
+
 import java.util.List;
 
 public interface NTxSimulationPlan {
+    NTxRendererContext rendererContext();
     NTxSolverRun add(String computeName, String solverName);
 
     void compile();
